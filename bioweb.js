27feb3fa72fd3de3bff1c3b2f89f15bc4ae037a2
unknown <clairebecker36@gmail.com>
2019-07-31 16:12:09 -0700

    profile

---
+++ bioweb.js
@@ -0,0 +1,25 @@
+// javascript file
+
+function flip(){
+  var test = document.getElementById('pool');
+  test.height="500";
+  test.width="500";
+  test.src="raft.jpg";
+}
+// function flip(){
+//   document.('STC.jpg').src= 'raft.jpg';
+// }
+function back(){
+  document.getElementById('pool').src='STC.jpg';
+}
+
+
+
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// <p> id=pool </p>
